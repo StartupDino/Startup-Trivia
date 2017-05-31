3a14331a8d11297662c0fc3a46b32e98eb79cb91
StartupDino <pnm326@gmail.com>
2017-05-31 15:55:37 -0400

    Added Questions struct file

---
+++ Startup Trivia/Questions.swift
@@ -0,0 +1,25 @@
+//
+//  Questions.swift
+//  Startup Trivia
+//
+//  Created by Pete McPherson on 5/31/17.
+//  Copyright © 2017 Pete McPherson. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+struct Question {
+    
+    let question: String
+    let answers: [Int: String]
+    let correctAnswer: Int
+    
+    func returnQuestion() -> String {
+        return question
+    }
+    
+    func returnAnswerNum() -> Int {
+        return correctAnswer
+    }
+}
