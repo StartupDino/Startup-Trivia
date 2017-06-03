efe5b8a89db7af159c024ba71e47057d26354aba
StartupDino <pnm326@gmail.com>
2017-06-03 15:55:57 -0400

    add more questions

--- Startup Trivia/ViewController.swift
+++ Startup Trivia/ViewController.swift
@@ -37,6 +37,8 @@ class ViewController: UIViewController {
     var questionsPerRound = 4
     var questionsAsked = 0
     var score = 0
+    var gameSound: SystemSoundID = 0
+
 
     
     // the data!
@@ -46,13 +48,16 @@ class ViewController: UIViewController {
         Question(question: "Elon Musk is from...", answers: [1: "South Africa", 2: "Hungary", 3: "The U.S.", 4: "Denmark"], correctAnswer: 1),
         Question(question: "Which of the following was NOT a founder of PayPal?", answers: [1: "Ken Howery", 2: "Elon Musk", 3: "Peter Thiel", 4: "Reid Hoffman"], correctAnswer: 4),
         Question(question: "What was the name of Tim Ferriss' first company that originally went on to inspire 'The Four Hour Work Week?'", answers: [1: "Uber", 2: "Drug Dealing for Fun And Profit", 3: "BrainQUICKEN", 4: "MetaBrain"], correctAnswer: 3),
-        Question(question: "The Shopify HQ's are located in what city?", answers: [1: "San Francisco", 2: "Los Angeles", 3: "Ottowa", 4: "Toronto"], correctAnswer: 3)
+        Question(question: "The Shopify HQ's are located in what city?", answers: [1: "San Francisco", 2: "Los Angeles", 3: "Ottowa", 4: "Toronto"], correctAnswer: 3),
+        Question(question: "What does MRR stand for?", answers: [1: "Main Rental Rooms", 2: "Monthly Recurring Revenue", 3: "Monthly Run Rate", 4: "Mark Read Red"], correctAnswer: 2),
+        Question(question: "Richard Branson has started this many companies...", answers: [1: "2", 2: "13", 3: "100", 4: "400+"], correctAnswer: 4)
     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         primeTheApp()
+        playGameStartSound()    
 
     }
 
